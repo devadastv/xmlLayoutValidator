615b7726512ff507b54a7a3b7c397fc715d3cfb6
Devadas Vijayan <devadas@devadas.(none)>
2013-04-17 10:18:56 +0200

    Added support for Text box and Arrows

--- src/com/xml/verifier/GadgetConfig.java
+++ src/com/xml/verifier/GadgetConfig.java
@@ -10,8 +10,8 @@ import java.awt.Color;
  *
  * @author devadas
  */
-public class GadgetConfig {
-
+public class GadgetConfig
+{
     public static final short GADGET_NOT_SUPPORTED = -1;
     public static final short VERTICAL_LIST = 0;
     public static final short HORIZONTAL_LIST = 1;
@@ -21,39 +21,58 @@ public class GadgetConfig {
     public static final short SELECTION_OVAL = 5;
     public static final short ACTION_HELP = 6;
     public static final short VEIL_RECTANGLE = 7;
-    
-    
+    public static final short TEXT_BOX = 8;
+    public static final short ARROWS = 9;
 
-    public static short getGadgetType(String gadgetName) {
-        if (gadgetName.equals("verticalList")) {
+    public static short getGadgetType(String gadgetName)
+    {
+        if (gadgetName.equals("verticalList"))
+        {
             return VERTICAL_LIST;
         }
-        if (gadgetName.equals("focusBox")) {
+        if (gadgetName.equals("focusBox"))
+        {
             return HORIZONTAL_LIST;
         }
-        if (gadgetName.equals("grid")) {
+        if (gadgetName.equals("grid"))
+        {
             return GRID;
         }
-        if (gadgetName.equals("focusBox")) {
+        if (gadgetName.equals("focusBox"))
+        {
             return FOCUS_BOX;
         }
-        if (gadgetName.equals("verticalBox")) {
+        if (gadgetName.equals("verticalBox"))
+        {
             return VERTICAL_BOX;
         }
-        if (gadgetName.equals("selectionOval")) {
+        if (gadgetName.equals("selectionOval"))
+        {
             return SELECTION_OVAL;
         }
-        if (gadgetName.equals("actionHelp")) {
+        if (gadgetName.equals("actionHelp"))
+        {
             return ACTION_HELP;
         }
-        if (gadgetName.equals("veilRectangle")) {
+        if (gadgetName.equals("veilRectangle"))
+        {
             return VEIL_RECTANGLE;
+        }
+        if (gadgetName.equals("textBox"))
+        {
+            return TEXT_BOX;
+        }
+        if (gadgetName.equals("arrows"))
+        {
+            return ARROWS;
         }
         return GADGET_NOT_SUPPORTED;
     }
 
-    public static Color getGadgetColor(short gadgetType) {
-        switch (gadgetType) {
+    public static Color getGadgetColor(short gadgetType)
+    {
+        switch (gadgetType)
+        {
             case VERTICAL_LIST:
                 return Color.red;
             case HORIZONTAL_LIST:
@@ -61,7 +80,7 @@ public class GadgetConfig {
             case FOCUS_BOX:
                 return Color.cyan;
             case GRID:
-                return Color.yellow;
+                return Color.cyan;
             case VERTICAL_BOX:
                 return Color.blue;
             case SELECTION_OVAL:
@@ -70,6 +89,10 @@ public class GadgetConfig {
                 return Color.ORANGE;
             case VEIL_RECTANGLE:
                 return Color.PINK;
+            case TEXT_BOX:
+                return Color.DARK_GRAY;
+            case ARROWS:
+                return Color.RED;
             default:
                 return Color.gray;
         }
